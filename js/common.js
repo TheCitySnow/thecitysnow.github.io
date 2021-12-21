622e00663b33386cd7d0873793c880db0529f5b0
TheCitySnow <yudequ321@gmail.com>
2021-12-21 08:39:38 +0800

    Site updated: 2021-12-21 08:39:38

--- js/common.js
+++ js/common.js
@@ -1,13 +1,33 @@
 $(function(){
+    
+    hashChange();
+    //监听URL地址变化
+    let oldHref = location.href;
     setInterval(function() {
-        if($(".index")) {
-            $(".index").parent().css("background-color", "transparent")
-            $(".index").parent().css("padding", "0")
+        let newHref = location.href;
+        if (oldHref !== newHref) {
+            let _oldHref = oldHref;
+            oldHref = newHref;
+            hashChange.call(window, {
+                'type': 'hashchange',
+                'newURL': newHref,
+                'oldURL': _oldHref
+            });
         }
-        if($(".post")) {
-            $(".post").parent().css("-webkit-box-shadow", "5px 5px 10px #ababab")
-            $(".post").parent().css("-moz-box-shadow", "5px 5px 10px #ababab")
-            $(".post").parent().css("border-radius", "10px")
-        }
-    }, 100)
+    }, 100);
 })
+
+let hashChange = () => {
+    if($(".index")) {
+        $(".index").parent().css("background-color", "transparent")
+        $(".index").parent().css("padding", "0")
+    }
+    if($(".post")) {
+        $(".post").parent().css("-webkit-box-shadow", "5px 5px 10px #ababab")
+        $(".post").parent().css("-moz-box-shadow", "5px 5px 10px #ababab")
+        $(".post").parent().css("border-radius", "10px")
+    }
+    $.each($(".post-body p br"),function(index, item){
+        $(item).after($("<span class='indent'></span>"));
+    })
+}
