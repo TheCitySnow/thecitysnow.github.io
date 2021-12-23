faaf05434cbc8be6bc3302c388547891e15bb476
TheCitySnow <yudequ321@gmail.com>
2021-12-23 10:51:22 +0800

    Site updated: 2021-12-23 10:51:21

--- js/common.js
+++ js/common.js
@@ -1,33 +1,3 @@
 $(function(){
-    
-    hashChange();
-    //监听URL地址变化
-    let oldHref = location.href;
-    setInterval(function() {
-        let newHref = location.href;
-        if (oldHref !== newHref) {
-            let _oldHref = oldHref;
-            oldHref = newHref;
-            hashChange.call(window, {
-                'type': 'hashchange',
-                'newURL': newHref,
-                'oldURL': _oldHref
-            });
-        }
-    }, 100);
+    console.log("ok");
 })
-
-let hashChange = () => {
-    if($(".index")) {
-        $(".index").parent().css("background-color", "transparent")
-        $(".index").parent().css("padding", "0")
-    }
-    if($(".post")) {
-        $(".post").parent().css("-webkit-box-shadow", "5px 5px 10px #ababab")
-        $(".post").parent().css("-moz-box-shadow", "5px 5px 10px #ababab")
-        $(".post").parent().css("border-radius", "10px")
-    }
-    $.each($(".post-body p br"),function(index, item){
-        $(item).after($("<span class='indent'></span>"));
-    })
-}
